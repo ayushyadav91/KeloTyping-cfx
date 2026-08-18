f105900201717d32361a03968ef7224fcb611116
parththakare2005 <parthdt2005@gmail.com>
2026-08-18 10:05:39 +0530

    Update auth API

--- src/utils/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
-
-const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>): RequestHandler =>
-  (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-
-export default asyncHandler;
